0a49aeac31b5cc24ca1e560bb8e3c79cc97cd8a2
Rakesh Gupta <rg688407@gmail.com>
2025-10-31 21:37:46 +0530

    optimal solution

---
+++ sol.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minNumberOperations(vector<int>& target) {
+
+    // using the optimal  solution.
+
+    int count = target[0];
+
+    for(int i = 1; i<target.size(); i++){
+        count += max((target[i]-target[i-1]),0);
+    }
+
+  return count;
+
+
+    }
+};
